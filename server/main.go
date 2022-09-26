7f0d512594db0acedffecc276b3b64c4de8de7bc
Jatin Kamboj <er.jatinkamboj96@gmail.com>
2022-09-26 22:57:33 +0530

    Containerised docker go app

---
+++ server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	// GET /api/register
+	app.Get("/api/*", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("✋ %s", c.Params("*"))
+		return c.SendString(msg) // => ✋ register
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
